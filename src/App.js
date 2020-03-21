cd012cd5da440b223c8b588cd0f4fdd57f965ce3
mading <849793164@qq.com>
2020-03-21 15:50:47 +0800

    项目启动

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Test from './test/box'
+import {Button}  from 'antd'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='box'>superfour</h1>
+      <Test></Test>
+      <Button>注册</Button>
+      <button>提交</button>
     </div>
   );
 }
+
 
 export default App;
